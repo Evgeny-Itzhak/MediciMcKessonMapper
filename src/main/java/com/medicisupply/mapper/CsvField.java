48fe7020824bdc5a8f24abd1c80adb242de4e9a8
evgeny.cherner <Evgeny.Cherner@openbet.com>
2025-05-29 04:25:20 +0300

    BUILD-0001: added default value in csv field mapping and processor

--- src/main/java/com/medicisupply/mapper/CsvField.java
+++ src/main/java/com/medicisupply/mapper/CsvField.java
@@ -6,15 +6,21 @@ import lombok.Getter;
 @Getter
 public enum CsvField {
     HANDLE("Handle"),
-    COMMAND("Command"),
+    COMMAND("Command", "MERGE"),
     TITLE("Title"),
     VENDOR("Vendor"),
     TESTCOLUMN ("TestColumn");
 
     private final String header;
+    private final String defaultValue;
 
     CsvField(String header) {
+        this(header, null);
+    }
+
+    CsvField(String header, String defaultValue) {
         this.header = header;
+        this.defaultValue = defaultValue;
     }
 
 }
--- src/main/java/com/medicisupply/mapper/FieldProcessor.java
+++ src/main/java/com/medicisupply/mapper/FieldProcessor.java
@@ -3,6 +3,8 @@ package com.medicisupply.mapper;
 
 import com.medicisupply.model.ProductRow;
 
+import static com.medicisupply.mapper.CsvField.COMMAND;
+
 public class FieldProcessor {
     public static String process(CsvField field, ProductRow row) {
         switch (field) {
@@ -13,7 +15,7 @@ public class FieldProcessor {
             case VENDOR:
                 return row.getField("Brand or Series");
             case COMMAND:
-                return "MERGE";
+                return COMMAND.getDefaultValue();
             default:
                 return row.getField(field.getHeader());
         }
